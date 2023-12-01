c77342ede2de82f57e527d81bceb92531a2319e4
ykh <781023956@qq.com>
2023-12-01 23:33:24 +0800

    almost done HTTPS Server

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
+#include <netinet/in.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <pthread.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <pthread.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include <stdarg.h>
+#include <unistd.h>
 
 #define HTTP_PORT 80
 #define HTTPS_PORT 443
 #define BUFFER_SIZE 1024
 
 // 日志颜色
-#define LOG_COLOR_RED     "\x1b[31m"
-#define LOG_COLOR_GREEN   "\x1b[32m"
-#define LOG_COLOR_YELLOW  "\x1b[33m"
-#define LOG_COLOR_BLUE    "\x1b[34m"
-#define LOG_COLOR_RESET   "\x1b[0m"
+#define LOG_COLOR_RED "\x1b[31m"
+#define LOG_COLOR_GREEN "\x1b[32m"
+#define LOG_COLOR_YELLOW "\x1b[33m"
+#define LOG_COLOR_BLUE "\x1b[34m"
+#define LOG_COLOR_RESET "\x1b[0m"
 
 // 日志级别
 typedef enum { DEBUG, INFO, WARN, ERROR } LogLevel;
@@ -26,183 +26,208 @@ typedef enum { DEBUG, INFO, WARN, ERROR } LogLevel;
 #define LOG_DEBUG 1
 
 void log_message(LogLevel level, const char *format, ...) {
-    #ifdef LOG_DEBUG
-    const char *color;
-    const char *level_strings[4] = {"DEBUG", "INFO", "WARN", "ERROR"};
-    switch(level) {
-        case DEBUG: color = LOG_COLOR_BLUE; break;
-        case INFO:  color = LOG_COLOR_GREEN; break;
-        case WARN:  color = LOG_COLOR_YELLOW; break;
-        case ERROR: color = LOG_COLOR_RED; break;
-        default: color = LOG_COLOR_RESET;
-    }
+#ifdef LOG_DEBUG
+  const char *color;
+  const char *level_strings[4] = {"DEBUG", "INFO", "WARN", "ERROR"};
+  switch (level) {
+  case DEBUG:
+    color = LOG_COLOR_BLUE;
+    break;
+  case INFO:
+    color = LOG_COLOR_GREEN;
+    break;
+  case WARN:
+    color = LOG_COLOR_YELLOW;
+    break;
+  case ERROR:
+    color = LOG_COLOR_RED;
+    break;
+  default:
+    color = LOG_COLOR_RESET;
+  }
 
-    printf("%s[%s] ", color, level_strings[level]);
+  printf("%s[%s] ", color, level_strings[level]);
 
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
+  va_list args;
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
 
-    printf("%s\n", LOG_COLOR_RESET);
-    #endif
+  printf("%s\n", LOG_COLOR_RESET);
+#endif
 }
 
 void init_openssl() {
-    SSL_load_error_strings();
-    OpenSSL_add_ssl_algorithms();
-    log_message(DEBUG, "OpenSSL initialized");
+  SSL_library_init();
+  SSL_load_error_strings();
+  OpenSSL_add_ssl_algorithms();
+  log_message(DEBUG, "OpenSSL initialized");
 }
 
-SSL_CTX *create_context() {
-    const SSL_METHOD *method;
-    SSL_CTX *ctx;
+SSL *get_new_ssl() {
+  const SSL_METHOD *method;
+  SSL_CTX *ctx;
 
-    method = SSLv23_server_method();
-    ctx = SSL_CTX_new(method);
-    if (!ctx) {
-        perror("Unable to create SSL context");
-        ERR_print_errors_fp(stderr);
-        log_message(ERROR, "Failed to create SSL context");
-        exit(EXIT_FAILURE);
-    }
+  method = SSLv23_server_method();
+  ctx = SSL_CTX_new(method);
+  if (!ctx) {
+    perror("Unable to create SSL context");
+    ERR_print_errors_fp(stderr);
+    log_message(ERROR, "Failed to create SSL context");
+    exit(EXIT_FAILURE);
+  }
 
-    log_message(DEBUG, "SSL context created");
-    return ctx;
-}
+  log_message(DEBUG, "SSL context created");
+  SSL_CTX_set_ecdh_auto(ctx, 1);
+  SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+  // Set the key and cert
 
-void configure_context(SSL_CTX *ctx) {
-    SSL_CTX_set_ecdh_auto(ctx, 1);
+  if (SSL_CTX_use_certificate_file(ctx, "keys/cnlab.cert", SSL_FILETYPE_PEM) <=
+      0) {
+    ERR_print_errors_fp(stderr);
+    log_message(ERROR, "Failed to load SSL certificate");
+    exit(EXIT_FAILURE);
+  }
 
-    // Set the key and cert
-    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        log_message(ERROR, "Failed to load SSL certificate");
-        exit(EXIT_FAILURE);
-    }
+  if (SSL_CTX_use_PrivateKey_file(ctx, "keys/cnlab.prikey", SSL_FILETYPE_PEM) <=
+      0) {
+    ERR_print_errors_fp(stderr);
+    log_message(ERROR, "Failed to load SSL private key");
+    exit(EXIT_FAILURE);
+  }
+  if (SSL_CTX_check_private_key(ctx) <= 0) {
+    // 检查私钥
+    printf("check private key error");
+    exit(1);
+  }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0 ) {
-        ERR_print_errors_fp(stderr);
-        log_message(ERROR, "Failed to load SSL private key");
-        exit(EXIT_FAILURE);
-    }
+  SSL *ssl = SSL_new(ctx);
 
-    log_message(DEBUG, "SSL context configured");
+  if (ssl == NULL) {
+    printf("SSL_new error");
+    exit(1);
+  }
+
+  log_message(DEBUG, "SSL context configured");
+  return ssl;
 }
 
 void handle_request(int client_socket, SSL *ssl) {
-    char buffer[BUFFER_SIZE];
-    int bytes_read;
+  char buffer[BUFFER_SIZE];
+  int bytes_read;
 
-    // Read the request
-    if (ssl) {
-        bytes_read = SSL_read(ssl, buffer, sizeof(buffer));
-    } else {
-        bytes_read = read(client_socket, buffer, sizeof(buffer));
-    }
-    buffer[bytes_read] = '\0';
+  // Read the request
+  if (ssl) {
+    bytes_read = SSL_read(ssl, buffer, sizeof(buffer));
+  } else {
+    bytes_read = read(client_socket, buffer, sizeof(buffer));
+  }
+  buffer[bytes_read] = '\0';
 
-    // Simple response for demonstration
-    char response_header[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-    char response_body[] = "<html><body><h1>Hello, World!</h1></body></html>";
+  // Simple response for demonstration
+  char response_header[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  char response_body[] = "<html><body><h1>Hello, World!</h1></body></html>";
 
-    if (ssl) {
-        SSL_write(ssl, response_header, strlen(response_header));
-        SSL_write(ssl, response_body, strlen(response_body));
-    } else {
-        write(client_socket, response_header, strlen(response_header));
-        write(client_socket, response_body, strlen(response_body));
-    }
+  if (ssl) {
+    SSL_write(ssl, response_header, strlen(response_header));
+    SSL_write(ssl, response_body, strlen(response_body));
+  } else {
+    write(client_socket, response_header, strlen(response_header));
+    write(client_socket, response_body, strlen(response_body));
+  }
 }
 
 void *connection_handler(void *data) {
-    int client_socket = *((int *)data);
-    handle_request(client_socket, NULL);
-    close(client_socket);
-    free(data);
-    return NULL;
+  int client_socket = *((int *)data);
+  handle_request(client_socket, NULL);
+  close(client_socket);
+  free(data);
+  return NULL;
 }
 
 void start_server(int port, SSL_CTX *ctx) {
-    int server_fd, *client_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    pthread_t thread_id;
+  int server_fd, *client_socket;
+  struct sockaddr_in address;
+  int addrlen = sizeof(address);
+  pthread_t thread_id;
 
-    // Create socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
+  // Create socket
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("Socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Define address and port
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(port);
+
+  // Bind socket
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("Bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Listen on socket
+  if (listen(server_fd, 10) < 0) {
+    perror("Listen failed");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    client_socket = malloc(sizeof(int));
+    *client_socket =
+        accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+
+    if (port == HTTPS_PORT) {
+      SSL *ssl = get_new_ssl(ctx);
+      if (!ssl) {
+        log_message(ERROR, "Failed to create SSL structure");
+        ERR_print_errors_fp(stderr);
+        // 处理错误，例如关闭socket，清理资源等
         exit(EXIT_FAILURE);
-    }
-
-    // Define address and port
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
-    // Bind socket
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
+      }
+      if (SSL_set_fd(ssl, *client_socket) == 0) {
+        log_message(ERROR, "Failed to set the file descriptor for SSL");
+        // 处理错误
         exit(EXIT_FAILURE);
+      }
+      if (SSL_accept(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+      } else {
+        handle_request(*client_socket, ssl);
+      }
+      SSL_free(ssl);
+    } else {
+      pthread_create(&thread_id, NULL, connection_handler, client_socket);
     }
+  }
 
-    // Listen on socket
-    if (listen(server_fd, 10) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1) {
-        client_socket = malloc(sizeof(int));
-        *client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-
-        if (port == HTTPS_PORT) {
-            SSL *ssl = SSL_new(ctx);
-	    if (!ssl) {
-	        log_message(ERROR, "Failed to create SSL structure");
-	        // 处理错误，例如关闭socket，清理资源等
-	    }
-	    if (SSL_set_fd(ssl, client_socket) == 0) {
-	        log_message(ERROR, "Failed to set the file descriptor for SSL");
-	        // 处理错误
-	    }
-            if (SSL_accept(ssl) <= 0) {
-                ERR_print_errors_fp(stderr);
-            } else {
-                handle_request(*client_socket, ssl);
-            }
-            SSL_free(ssl);
-        } else {
-            pthread_create(&thread_id, NULL, connection_handler, client_socket);
-        }
-    }
-
-    close(server_fd);
+  close(server_fd);
 }
 
 int main() {
-    SSL_CTX *ctx;
+  SSL_CTX *ctx;
 
-    log_message(INFO, "Server starting...");
-    init_openssl();
-    ctx = create_context();
-    configure_context(ctx);
+  log_message(INFO, "Server starting...");
+  init_openssl();
 
-    pthread_t thread_id_http, thread_id_https;
+  pthread_t thread_id_http, thread_id_https;
 
-    // Start HTTP server
-    pthread_create(&thread_id_http, NULL, (void *)start_server, (void *)HTTP_PORT);
+  // Start HTTP server
+  pthread_create(&thread_id_http, NULL, (void *)start_server,
+                 (void *)HTTP_PORT);
 
-    // Start HTTPS server
-    pthread_create(&thread_id_https, NULL, (void *)start_server, (void *)HTTPS_PORT);
+  // Start HTTPS server
+  pthread_create(&thread_id_https, NULL, (void *)start_server,
+                 (void *)HTTPS_PORT);
 
-    pthread_join(thread_id_http, NULL);
-    pthread_join(thread_id_https, NULL);
+  pthread_join(thread_id_http, NULL);
+  pthread_join(thread_id_https, NULL);
 
-    SSL_CTX_free(ctx);
-    //cleanup_openssl();
+  // SSL_CTX_free(ctx);
+  // cleanup_openssl();
 
-    log_message(INFO, "Server stopping...");
-    return 0;
+  log_message(INFO, "Server stopping...");
+  return 0;
 }
-
